b4cf25ecd86fcddd2167f9ab0ba6c4e2a72ce4c3
Francois Roland <froland@froland.be>
2021-01-08 11:12:09 +0100

    Tune scripts.

--- install_app.sh
+++ install_app.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+echo "Installing Node.js"
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+node -e "console.log('Running Node.js ' + process.version)"
+
+echo "Installing demo-nodejs with PM2"
+
 npm install
 npm start
 npm run startup
+
+echo "DONE"
+echo "-- Don't forget to run the startup script."
