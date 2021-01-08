3aa4b023683e96ea7d21f7c481ead2dea0044161
Francois Roland <froland@froland.be>
2021-01-08 11:29:34 +0100

    Give instructions

--- install_app.sh
+++ install_app.sh
@@ -10,10 +10,10 @@ node -e "console.log('Running Node.js ' + process.version)"
 echo "Installing demo-nodejs with PM2"
 
 npm install
-npm start
 
 echo "DONE"
 echo "To complete:"
 echo " - Reload your bash config with '. ~/.bashrc'"
 echo " - Run 'npm run pm2startup'"
+echo " - Run 'npm start'"
 echo " - Run 'npm run pm2save'"
